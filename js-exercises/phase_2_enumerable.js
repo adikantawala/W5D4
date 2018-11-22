7b23c6a9523f7adab58e884a272c88da0418685a
adikantawala <adikan@ucla.edu>
2018-11-22 15:01:23 -0800

    Complete phase 2 of JS JS exercises

---
+++ js-exercises/phase_2_enumerable.js
@@ -0,0 +1,52 @@
+Array.prototype.myEach = function(callback) {
+  this.forEach(callback);
+  return this;
+};
+function printNum(el) {
+  console.log(el);
+}
+
+// console.log([1,2,3,4].myEach(printNum));
+
+
+Array.prototype.myMap = function(callback) {
+  let results = [];
+  
+  let mapCallback = (el) => {
+    results.push(callback(el));
+  };
+  
+  this.myEach(mapCallback);
+  return results;
+};
+
+// console.log([1,2,3,4].myMap((el) => el * 2));
+
+Array.prototype.myReduce = function(callback, accumulator = null) {
+  const temp = [...this];
+  if (accumulator === null) {
+    accumulator = temp.shift();
+  }
+  
+  const reduceCallback = (el) => {
+    accumulator = callback(accumulator,el);
+  };
+
+  
+  temp.myEach(reduceCallback); 
+  return accumulator;
+  
+};
+
+re1 = [1, 2, 3].myReduce(function(acc, el) {
+  return acc + el;
+}); // => 6
+
+// console.log(re1);
+
+// with initialValue
+re2 = [1, 2, 3].myReduce(function(acc, el) {
+  return acc + el;
+}, 25); // => 31
+console.log(re2);
+

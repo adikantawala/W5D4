eead82396245b676bea88c1f2a31790ea13f94f3
adikantawala <adikan@ucla.edu>
2018-11-22 15:18:52 -0800

    Finish phase 3

---
+++ js-exercises/phase_3_iteration.js
@@ -0,0 +1,33 @@
+Array.prototype.bubbleSort = function() {
+  
+  let check = true;
+  let resultArr = [...this];
+  while (check) {
+    check = false;
+    
+    for (let i = 0; i < resultArr.length - 1; i++) {
+      if (resultArr[i] > resultArr[i + 1]) {
+        let tmp = resultArr[i + 1];
+        resultArr[i + 1] = resultArr[i];
+        resultArr[i] = tmp;
+        check = true;
+      }
+    }
+  }
+  return resultArr;
+};
+
+console.log([1,3,5,7,8,3,2].bubbleSort());
+
+String.prototype.substrings = function() {
+  let resultArr = [];
+
+  for (var i = 0; i < this.length; i++) {
+    for (var j = i + 1; j <= this.length; j++) {
+      resultArr.push(this.slice(i,j));
+    }
+  }
+  return resultArr;
+};
+
+console.log("cat".substrings());
